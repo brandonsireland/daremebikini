03ff44135455bf510412373d12b7f3c7714e2544
Brandon Ireland <brandonsireland@gmail.com>
2018-06-19 00:19:36 +0200

    cleaned header scss, started removal of bootstrap, started implementation of flexbox.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 (function(){
    
     //Swiper Carousel
-    var mySwiper = new Swiper('.swiper-container', {
+    var headerSwiper = new Swiper('.swiper-container', {
         spaceBetween: 30,
         effect: 'fade',
         loop: true,
@@ -19,32 +19,32 @@
     var carouselSwiper = new Swiper('.s2', {
         slidesPerView: 4,
         loop: true,
-        autoplay: {
-            delay:2000,
-            disableOnInteraction:false,
-        },
+        // autoplay: {
+        //     delay:2000,
+        //     disableOnInteraction:false,
+        // },
         navigation: {
             nextEl: '.swiper-button-next2',
             prevEl: '.swiper-button-prev2',
         },
-        breakpoints: {
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            }
-        }
+        // breakpoints: {
+        //     1024: {
+        //         slidesPerView: 3,
+        //         spaceBetween: 40,
+        //     },
+        //     768: {
+        //         slidesPerView: 3,
+        //         spaceBetween: 30,
+        //     },
+        //     640: {
+        //         slidesPerView: 2,
+        //         spaceBetween: 20,
+        //     },
+        //     320: {
+        //         slidesPerView: 1,
+        //         spaceBetween: 10,
+        //     }
+        // }
     });
 
 //Fade in Header Text
@@ -54,53 +54,20 @@ if (el.classList.contains('is-paused')) {
     el.classList.remove('is-paused');
 }
 
-// Mobile Navigation Side Buttons
-var mobiNav = false;
-function mobileNav() {
-    var x = document.getElementById("mobile-nav-icon");
-    x.classList.toggle('open');
-    if (x.classList.contains('open')) {
-        openNav();
-    } else {
-        closeNav();
-    }
+// // Mobile Navigation Side Button
 
-};
+const sidenav = document.querySelector('#sideNav');
+const navButton = document.querySelector('#mobileNavButton');
+navButton.addEventListener('click', clickNav = () => {
+    navButton.classList.toggle('open');
+    sidenav.classList.toggle('open');
+});
 
-function openNav() {
-    this.mobileNav = mobileNav;
-    document.getElementById("mySidenav").style.width = "100%";
-}
-
-function closeNav() {
-    this.mobileNav = false;
-    document.getElementById("mySidenav").style.width = "0";
-}
 
 // Waypoint animations
-
-var animatedPhotoOne = document.getElementById('animatePhoto1');
-var animatedPhotoTwo = document.getElementById('animatePhoto2');
 var animatedShopSection = document.getElementById('animateShop');
 var animatedInstaSection = document.getElementById('animateShopLook');
 
-var waypoint = new Waypoint({
-   element: document.getElementById('animatePhoto1'),
-   handler: function (direction) {
-      animatedPhotoOne.classList.add('animated','fadeInUp');
-   },
-   offset: '70%'
-   
-});
-
-var waypoint2 = new Waypoint({
-  element: document.getElementById('animatePhoto2'),
-  handler: function (direction) {
-     animatedPhotoTwo.classList.add('animated','fadeInUp');
-  },
-  offset: '60%'
-  
-});
 
 var waypointThree = new Waypoint({
    element: document.getElementById('animateShop'),
